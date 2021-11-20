fa388ca9664d2c114c5f8ae365ec1793dfc87fc0
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-11-20 16:02:22 -0600

    server up, added index.js, seeds work

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-// need to setup server
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 
+// routes
+app.use(require("./routes/api.js"));
 
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
 
 
 
